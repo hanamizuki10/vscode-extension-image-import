c0685e01c772bd75e6e24d5e92a7ed56ff432ca0
hanamizuki10 <mizuki.hana@imprich-create.site>
2023-10-20 17:16:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# vscode-image-import README
+
+## 開発の流れ
+- ターミナル: `npm install`
+- ターミナル: `npm run watch`
+- `F5` キー押下でデバッグ実行
+- 別ウィンドウでVSCodeが起動されるため、以下のコマンドを実行して動き確認する
+  - `Panel View My Love Cat`: テキストエディタにパネルを追加して画像を表示する
+  - `Explorer View My Love Cat`: エクスプローラーに画像を表示する
+  - `View My Love Cat Random Update`: 表示中の画像をランダムに変更する
+
+## 技術
+ - Webview
---
+++ src/extension.ts
@@ -0,0 +1,195 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  //==========================================================================
+  // まずシンプルにエディタに表示 - start
+  //==========================================================================
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vscode-image-import.viewPanelMyLoveCat', () => {
+      MyLoveCatViewPanel.createOrShow(context.extensionUri);
+    })
+  );
+  //==========================================================================
+  // まずシンプルにエディタに表示 - end
+  //==========================================================================
+  //==========================================================================
+  // explorer に表示 - start
+  //==========================================================================
+  context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(MyLoveCatViewProvider.viewType, new MyLoveCatViewProvider(context.extensionUri))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vscode-image-import.viewExplorerMyLoveCat', () => {
+      // すでにエクスプローラーには情報を表示しているけど、フォーカスを当てる感じ
+      // 隠れていたら展開して表示するしエクスプローラーを閉じていたら開く
+      vscode.commands.executeCommand('viewExplorerMyLoveCat.focus');
+    })
+  );
+  //==========================================================================
+  // explorer に表示 - end
+  //==========================================================================
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vscode-image-import.viewMyLoveCatRandom', () => {
+      // 描画されている猫をrandomで描画切り替え
+      MyLoveCatViewPanel.randomUpdate();
+      MyLoveCatViewProvider.randomUpdate();
+    })
+  );
+}
+
+class MyLoveCatViewPanel {
+	public static currentPanel: MyLoveCatViewPanel | undefined;
+  private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionUri: vscode.Uri;
+	public static readonly viewType = 'viewTypePanel';
+	private _disposables: vscode.Disposable[] = [];
+
+	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		MyLoveCatViewPanel.currentPanel = new MyLoveCatViewPanel(panel, extensionUri);
+	}
+  public static randomUpdate() {
+    if (MyLoveCatViewPanel.currentPanel) {
+			MyLoveCatViewPanel.currentPanel._update();
+			return;
+		}
+  }
+
+  public static createOrShow(extensionUri: vscode.Uri) {
+    // TODO:この引数の意味をしっかりと後で確認する
+    const column = vscode.window.activeTextEditor
+    ? vscode.window.activeTextEditor.viewColumn
+    : undefined;
+    // すでにパネルがある場合はそれを見せる
+		if (MyLoveCatViewPanel.currentPanel) {
+			MyLoveCatViewPanel.currentPanel._panel.reveal(column);
+			return;
+		}
+
+    console.log(column);
+    const webviewType = 'viewTypePanel';
+    const webviewTitle = 'My Love Cat';
+    // TODO:何ができるのか後で確認する
+    const webviewOptions:vscode.WebviewOptions = {
+      // Webview で JavaScript を有効にする
+      enableScripts: true,
+    };
+    const panel = vscode.window.createWebviewPanel(
+      webviewType,
+      webviewTitle,
+      column || vscode.ViewColumn.One,
+      webviewOptions,
+    );
+		MyLoveCatViewPanel.currentPanel = new MyLoveCatViewPanel(panel, extensionUri);
+  }
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+		this._extensionUri = extensionUri;
+    this._update();
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Update the content based on view changes
+		this._panel.onDidChangeViewState(
+			e => {
+				if (this._panel.visible) {
+					this._update();
+				}
+			},
+			null,
+			this._disposables
+		);
+  }
+  public dispose() {
+		MyLoveCatViewPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+  private _update() {
+    const images = ["MyLoveCat01.jpg","MyLoveCat02.jpg","MyLoveCat03.jpg"];
+    let viewFileName = images[Math.floor(Math.random() * images.length)];
+		const webview = this._panel.webview;
+		this._panel.webview.html = this._getHtmlForWebview(webview, viewFileName);
+  }
+	private _getHtmlForWebview(webview: vscode.Webview, filename: string) {
+    /** ファイルパスを生成 */
+    const catImageUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', filename));
+    /** ファイルパスのURIを生成 */
+    const catGifSrc = webview.asWebviewUri(catImageUri);
+    return  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>My Love Cat</title>
+</head>
+<body>
+  <p>${filename}</p>
+  <img src="${catGifSrc}"></img>
+</body>
+</html>`;
+  }
+}
+
+class MyLoveCatViewProvider implements vscode.WebviewViewProvider {
+	public static currentProvider: MyLoveCatViewProvider | undefined;
+  public static readonly viewType = 'viewExplorerMyLoveCat';
+	private _view?: vscode.WebviewView;
+  public static randomUpdate() {
+    if (MyLoveCatViewProvider.currentProvider) {
+			MyLoveCatViewProvider.currentProvider._update();
+			return;
+		}
+  }
+  constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) {
+    MyLoveCatViewProvider.currentProvider = this;
+  }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+		webviewView.webview.options = {
+			enableScripts: true
+		};
+    this._update();
+	}
+
+  private _update() {
+    if (this._view) {
+      const images = ["MyLoveCat01.jpg","MyLoveCat02.jpg","MyLoveCat03.jpg"];
+      let viewFileName = images[Math.floor(Math.random() * images.length)];
+      const webview = this._view.webview;
+      this._view.webview.html = this._getHtmlForWebview(webview, viewFileName);
+    }
+  }
+
+	private _getHtmlForWebview(webview: vscode.Webview, filename: string) {
+    /** ファイルパスを生成 */
+    const catImageUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', filename));
+    /** ファイルパスのURIを生成 */
+    const catGifSrc = webview.asWebviewUri(catImageUri);
+    return  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>My Love Cat</title>
+</head>
+<body>
+  <p>${filename}</p>
+  <img src="${catGifSrc}"></img>
+</body>
+</html>`;
+  }
+}
